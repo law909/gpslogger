4ed1feb40ab87d9f433328bf6e034abf91e27815
law909 <balint.lovey@gmail.com>
2026-03-20 01:15:07 +0100

    Remove `/time-test` route

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('home');
 Route::view('/map', 'pages.location-map')->name('location-map');
 
-Route::get('/time-test', function () {
-    echo "Szerver (PHP) ideje: " . date('Y-m-d H:i:s') . "<br>";
-    echo "Laravel (Carbon) ideje: " . \Carbon\Carbon::now()->toDateTimeString() . "<br>";
-    echo "Beállított időzóna: " . config('app.timezone') . "<br>";
-    echo "PHP default timezone: " . date_default_timezone_get();
-});
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
